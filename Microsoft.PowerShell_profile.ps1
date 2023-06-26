5afaa2f12ada28aa4bfe03ed5eebb40b5709822e
Bonito <nikhildhiman9050@gmail.com>
2023-06-26 18:26:12 +0530

    uploaded config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+$PSStyle.FileInfo.Directory = "`e[38;2;255;255;255m"
+
+
+function prompt {
+    $path = Get-Location
+    $directory = Split-Path -Leaf $path
+    $gitBranch = & git rev-parse --abbrev-ref HEAD 2>$null
+    $gitStatus = & git status --porcelain 2>$null
+
+    # Define custom colors
+    $promptColor = "White"
+    $directoryColor = "Cyan"
+    $gitBranchColor = "Yellow"
+    $gitStatusColor = "Red"
+
+    # Build the prompt string
+    $promptString = "$directory"
+    if ($gitBranch) {
+        $promptString += " <$gitBranch>"
+        if ($gitStatus) {
+            $unsavedChanges = ($gitStatus | Measure-Object).Count
+            $promptString += " [$unsavedChanges]"
+        }
+    }
+
+    # Customize the prompt color
+    $promptColorCode = "1;$([int]($promptColor -as [System.ConsoleColor]))"
+    $directoryColorCode = "1;$([int]($directoryColor -as [System.ConsoleColor]))"
+    $gitBranchColorCode = "1;$([int]($gitBranchColor -as [System.ConsoleColor]))"
+    $gitStatusColorCode = "1;$([int]($gitStatusColor -as [System.ConsoleColor]))"
+
+    $promptString = "`e[0;${promptColorCode}m`n`n$promptString"
+    $promptString += "`e[0;${promptColorCode}m`n`n> `e[0m"
+
+    Write-Host -NoNewLine $promptString
+
+    return " "
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# PowerShell Config
+
+Normal
+
+```txt
+directory
+
+>
+```
+
+In git directories
+
+```txt
+directory <branch> [uncommitted-files]
+
+>
+```
+
+## Setup
+
+- Clone this repo
+- Move the `Microsoft.PowerShell_profile.ps1` file to `$PROFILE` env path
+  - run `echo $PROFILE` to check current profile-path
